2b826a602ed9944c91f6e1123efa03722dc68e49
Alek90 <4lek.90@gmail.com>
2021-11-18 17:19:14 +0100

    Clone code and readjust for tvshow.
    Clonato codice e riadattato per mostrare i telefilm.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     
     <div class="search">
 
-      <input @keyup="createAPI()" v-model="titleRequired" type="text" placeholder="Titolo">
+      <input @keyup="createAPI(titleRequired)" v-model="titleRequired" type="text" placeholder="Titolo">
 
-      <button @click="callAPI ()">
+      <button @click="callAPI()">
         Cerca
       </button>
 
@@ -15,7 +15,9 @@
 
       <div class="container">
 
-        <div v-for="result in resultsFound" :key="result.id" class="film">
+        <h2>FILM</h2>
+
+        <div v-for="result in resultsFoundMovies" :key="result.id" class="film">
 
           <h2>{{result.title}}</h2>
 
@@ -25,6 +27,20 @@
 
           <p>{{result.vote_average}}</p>
 
+        </div>
+
+
+        <h2>TELEFILM</h2>
+
+        <div v-for="result in resultsFoundTvShow" :key="result.id" class="tvshow">
+
+          <h2>{{result.name}}</h2>
+
+          <p>{{result.original_name}}</p>
+
+          <p>{{result.original_language}}</p>
+
+          <p>{{result.vote_average}}</p>
 
         </div>
 
@@ -51,16 +67,17 @@ export default {
 
       titleRequired: '',
 
-      baseApi: ['https://api.themoviedb.org/3/search/movie?api_key=3d29c9625fadd02d13b86c0f4b58f8b7&language=it&page=1&include_adult=false&query='],
+      baseApiMovies: ['https://api.themoviedb.org/3/search/movie?api_key=3d29c9625fadd02d13b86c0f4b58f8b7&language=it-IT&page=1&include_adult=false&query='],
 
-      apiRequired: '',
+      apiRequiredMovies: '',
 
-      resultsFound: ''
+      resultsFoundMovies: '',
 
-      /* la query all'interno del'API deve essere inserita tramite collegamento con il v-model dell'input, così da determinare la richiesta completa di volta in volta. 
-      Punto di partenza:
-        -Come inserisco la stringa del v-model nella query?
-       */
+      baseApiTvShow: 'https://api.themoviedb.org/3/search/tv?api_key=3d29c9625fadd02d13b86c0f4b58f8b7&language=it-IT&page=1&include_adult=false&query=',
+
+      apiRequiredTvShow: '',
+
+      resultsFoundTvShow: ''
 
     }
   },
@@ -69,21 +86,31 @@ export default {
 
   methods:{
 
-    /* creazione dell'API in apiRequired */
-    createAPI() {
-      this.apiRequired = this.baseApi + this.titleRequired;
-      console.log(this.apiRequired);
+    /* creazione dell'API in apiRequiredMovies */
+    createAPI(required) {
+      this.apiRequiredMovies = this.baseApiMovies + required;
+      this.apiRequiredTvShow = this.baseApiTvShow + required;
+      /* console.log(this.apiRequiredMovies); */
+      /* console.log(this.apiRequiredTvShow); */
     },
 
     /* chiamata dell'API */
     callAPI () {
-      axios.get(this.apiRequired)
+      axios
+      .get(this.apiRequiredMovies)
       .then((response) => {
         console.log((response.data.results));
-        this.resultsFound = response.data.results;
-        console.log(this.resultsFound);
-      })
-    }
+        this.resultsFoundMovies = response.data.results
+      });
+      axios
+      .get(this.apiRequiredTvShow)
+      .then((response) => {
+        console.log(response.data.results);
+        this.resultsFoundTvShow = response.data.results
+      });
+
+    },
+
   }
 }
 </script>
---
+++ src/components/MainSite.vue
@@ -0,0 +1,6 @@
+<template>
+    <main id="site_main">
+        
+
+    </main>
+</template>
