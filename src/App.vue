b396dd841e8e0ae23cd42cc9a6be40bb5bcccc7b
Alek90 <4lek.90@gmail.com>
2021-11-18 15:07:46 +0100

    Create  container contenent the first page of result.
    Creato ciclo per mostrare a schermo i risultati della prima pagina.

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,27 @@
 
     </div>
 
+    <main id="site_main">
+
+      <div class="container">
+
+        <div v-for="result in resultsFound" :key="result.id" class="film">
+
+          <h2>{{result.title}}</h2>
+
+          <p>{{result.original_title}}</p>
+
+          <p>{{result.original_language}}</p>
+
+          <p>{{result.vote_average}}</p>
+
+
+        </div>
+
+      </div>
+
+    </main>
+
   </div>
 </template>
 
@@ -23,15 +44,20 @@ export default {
   components: {
   },
 
+
+
   data() {
     return {
 
-      titleRequired: 'jurassic',
+      titleRequired: '',
 
-      baseApi: ['https://api.themoviedb.org/3/search/movie?api_key=3d29c9625fadd02d13b86c0f4b58f8b7&language=en-US&page=1&include_adult=false&query='],
+      baseApi: ['https://api.themoviedb.org/3/search/movie?api_key=3d29c9625fadd02d13b86c0f4b58f8b7&language=it&page=1&include_adult=false&query='],
 
-      apiRequired: ''
-      /* la query all'interno del'API deve essere inserita tramite dollegamento con il v-model dell'input, così da determinare la richiesta completa di volta in volta. 
+      apiRequired: '',
+
+      resultsFound: ''
+
+      /* la query all'interno del'API deve essere inserita tramite collegamento con il v-model dell'input, così da determinare la richiesta completa di volta in volta. 
       Punto di partenza:
         -Come inserisco la stringa del v-model nella query?
        */
@@ -39,17 +65,23 @@ export default {
     }
   },
 
+
+
   methods:{
 
+    /* creazione dell'API in apiRequired */
     createAPI() {
       this.apiRequired = this.baseApi + this.titleRequired;
       console.log(this.apiRequired);
     },
 
+    /* chiamata dell'API */
     callAPI () {
       axios.get(this.apiRequired)
       .then((response) => {
         console.log((response.data.results));
+        this.resultsFound = response.data.results;
+        console.log(this.resultsFound);
       })
     }
   }
