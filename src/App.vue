0386e4acd16d67a9b22788d9d7e7bb86019fe1cc
Alek90 <4lek.90@gmail.com>
2021-11-18 12:58:13 +0100

    Create guide.
    Commentato ragionamento con obbiettivo post tentativi scorretti.

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,25 @@ export default {
   components: {
   },
 
+  data() {
+    return {
+
+      titleRequired: 'jurassic',
+
+      apiRequired: 'https://api.themoviedb.org/3/search/movie?api_key=3d29c9625fadd02d13b86c0f4b58f8b7&language=en-US&page=1&query=jurassic&include_adult=false'
+
+      /* la query all'interno del'API deve essere inserita tramite dollegamento con il v-model dell'input, così da determinare la richiesta completa di volta in volta. 
+      Punto di partenza:
+        -Come inserisco la stringa del v-model nella query?
+       */
+
+    }
+  },
+
   methods:{
+
     callAPI () {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=3d29c9625fadd02d13b86c0f4b58f8b7&language=en-US&page=1&query=jurassic&include_adult=false')
+      axios.get(this.apiRequired)
       .then((response) => {
         console.log((response.data.results));
       })
