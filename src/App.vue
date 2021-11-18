802a09be1c88cfcb3694c92263f826101236e8c7
Alek90 <4lek.90@gmail.com>
2021-11-18 12:31:19 +0100

    Create callAPI for test.
    Creata callAPI inserendo un' API appositamente costruita al fine di eseguire un test.

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,31 @@
 
       <input type="text" placeholder="Titolo">
 
-      <button>
+      <button @click="callAPI">
         Cerca
       </button>
-      
+
     </div>
 
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 
 export default {
   name: 'App',
   components: {
+  },
+
+  methods:{
+    callAPI () {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=3d29c9625fadd02d13b86c0f4b58f8b7&language=en-US&page=1&query=jurassic&include_adult=false')
+      .then((response) => {
+        console.log((response.data.results));
+      })
+    }
   }
 }
 </script>
